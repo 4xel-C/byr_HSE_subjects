008e7249234b3000435381e1dc74d37af02c7d15
4xel-C <axel-cano@live.fr>
2025-02-01 14:12:15 +0100

    implementing continue if user input wrong choice

--- main.py
+++ main.py
@@ -30,11 +30,11 @@ def main():
                 console.print("3--Q3 (Jul - Sep)")
                 console.print("4--Q4 (Oct - Dec)")
 
-                selection = input("\nQuarter number: ")
+                selection_start = input("\nQuarter number: ")
 
                 try:
-                    if 0 < int(selection) < 5:
-                        quarter = selection
+                    if 0 < int(selection_start) < 5:
+                        quarter = selection_start
                         current_menu = "main"
                     else:
                         raise ValueError
@@ -43,14 +43,33 @@ def main():
 
             
             case "main":
-                console.print("[bold green]=== Main menu ===")
-                console.print("1 - Generate ")
+                console.print("\n[bold green]=== Main menu ===")
+                console.print(f"\nGenerate the discussion file for Q{quarter}: Choose an option\n")
+                console.print("1 - Generate the file automaticly")
+                console.print("2 - Cherry pick each procedure")
+                console.print("3 - Choose another quarter")
+                
+                selection_main = input()
+                
+                try:
+                    if 0 < int(selection_main) < 4:
+                        quarter = selection_main
+                        current_menu = "main"
+                    else:
+                        raise ValueError
+                except ValueError:
+                    input("Invalid slection. Please enter a number between 1 and 3")
+                    continue
+                
+                print("testing the continue bypass")
+                    
+                
 
 
                 # testing part
-                for proc in procedures.list:
-                    print(proc.number, proc.part if proc.part else "")
-                break
+                # for proc in procedures.list:
+                #     print(proc.number, proc.part if proc.part else "")
+                # break
 
 
 if __name__ == "__main__":
